a613250d0edebd21cf0616b35a6b2ea3dcc0e225
Mahmoud <elmahallawy6@gmail.com>
2025-01-04 19:56:07 +0200

    customize admin.py for user model

--- E_commerce/app/accounts/admin.py
+++ E_commerce/app/accounts/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
+from .models import User
+from django.contrib.auth.admin import UserAdmin
 
-# Register your models here.
+@admin.register(User)
+class CustomUserAdmin(UserAdmin):
+    list_display = ('username', 'email', 'first_name', 'last_name', 'phone', 'is_active', 'is_staff', 'created_at')
+    list_filter = ('is_active', 'is_staff', 'is_superuser', 'created_at')
+    search_fields = ('email', 'username', 'first_name', 'last_name', 'phone')
+    ordering = ['-created_at']
+
+    fieldsets = (
+        (None,{"fields":('email', 'username', 'password')}),
+        ("personal info",{'fields':('first_name', 'last_name', 'phone', 'address')}),
+        ('permissions', {'fields':("is_staff", "is_active", "is_superuser", "groups", "user_permissions")}),
+        ("important dates",{"fields" : ("last_login", "created_at", "updated_at")}),
+                 )
+    
+
+    readonly_fields = ("created_at", "updated_at")
