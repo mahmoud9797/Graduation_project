671ec0c504f4e6e42e9e954082b93c7dd7150264
Mahmoud <elmahallawy6@gmail.com>
2025-01-04 16:04:08 +0200

    adding order model.py

--- E_commerce/app/orders/models.py
+++ E_commerce/app/orders/models.py
@@ -1,3 +1,40 @@
 from django.db import models
+from django.conf import settings
+from app.products.models import Products
 
-# Create your models here.
+
+class Order(models.Model):
+
+    STATUS_CHOICES = (
+        ('pending', 'Pending'),
+        ('processing', 'Processing'),
+        ('shipped', 'Shipped'),
+        ('delivered', 'Delivered'),
+        ('cancelled', 'Cancelled')
+    )
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="orders")
+    total_amount = models.DecimalField(max_digits=12, decimal_places=2)
+    status = models.CharField(max_length=20, choices=STATUS_CHOICES, default="pending")
+    shipping_address = models.TextField()
+    created_at = models.DateField(auto_now_add=True)
+    updated_at = models.DateField(auto_now=True)
+
+    class Meta:
+        ordering = ["-created_at"]
+        db_table = "orders"
+    
+    def __str__(self):
+        return f"order {self.id} by {self.user.email}"
+    
+class OrderItem(models.Model):
+    order = models.ForeignKey(Order, on_delete=models.CASCADE, related_name="items")
+    Products = models.ForeignKey(Products, on_delete=models.CASCADE)
+    quantity = models.ImageField()
+    price = models.DecimalField(max_digits=12, decimal_places=2)
+    created_at = models.DateField(auto_now_add=True)
+
+    class Meta:
+        db_table = "order_items"
+
+    def __str__(self):
+        return f"{self.quantity} x {self.Products.name}"
