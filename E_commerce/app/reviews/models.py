df45f4d519036514269b75b57f88d275c548a306
Mahmoud <elmahallawy6@gmail.com>
2025-01-04 16:15:43 +0200

    adding RATING_CHOICES to review models.py

--- E_commerce/app/reviews/models.py
+++ E_commerce/app/reviews/models.py
@@ -5,10 +5,18 @@ from app.accounts.models import User
 
 
 class Reviews(models.Model):
+
+    RATING_CHOICES = (
+        (1, "1 star"),
+        (2, "2 stars"),
+        (3, "3 stars"),
+        (4, "4 stars"),
+        (5, "5 stars")
+    )
     id = models.BigAutoField(null=False, primary_key=True)
     product = models.ForeignKey(Products, on_delete=models.CASCADE, related_name='reviews', null=False)
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='reviews', null=False)
-    rating = models.PositiveIntegerField(null=False)
+    rating = models.IntegerField(choices=RATING_CHOICES)
     comment = models.TextField(null=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
