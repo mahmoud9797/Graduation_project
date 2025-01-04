d2d000166bc08e2c5ff8d850cd933385645321d2
Mahmoud <elmahallawy6@gmail.com>
2025-01-04 16:34:57 +0200

    fix user model.py

--- E_commerce/app/accounts/models.py
+++ E_commerce/app/accounts/models.py
@@ -6,7 +6,7 @@ class User(AbstractUser):
     phone = models.CharField(max_length=15, blank=True)
     address = models.TextField(blank=True)
     created_at = models.DateField(auto_now_add=True)
-    updated_at = models.DateField(auto_created=True)
+    updated_at = models.DateField(auto_now=True)
 
     class Meta:
         ordering = ['-created_at']
