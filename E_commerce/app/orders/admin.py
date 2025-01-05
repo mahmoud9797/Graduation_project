a2cb95d30992944996e08a523726adf435e4b70b
Mahmoud <elmahallawy6@gmail.com>
2025-01-05 16:01:03 +0200

    3

--- E_commerce/app/orders/admin.py
+++ E_commerce/app/orders/admin.py
@@ -1,3 +1,25 @@
 from django.contrib import admin
+from .models import Order, OrderItem
 
-# Register your models here.
+class OrderItemInline(admin.TabularInline):
+    model = OrderItem
+    extra = 0
+    readonly_fields = ("created_at",)
+    raw_id_fields = ('Products',)
+
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ("id", "user", 'total_amount', "status", "created_at")
+    list_filter = ("status", "created_at")
+    search_fields = ("user__email", 'user__username', "shipping_address")
+    readonly_fields = ("created_at", "updated_at")
+    inlines = [OrderItemInline]
+
+    fieldsets = ((None,{"fields": ("user", "status", "total_amount")}),
+                 ('shipping information',{"fields":("shipping_address",)}),
+                 ("Timestamps",{"fields":("created_at", "updated_at"),
+                                "classes": ("collapse",)
+                                }),
+                 
+                 )
+
--- E_commerce/app/orders/models.py
+++ E_commerce/app/orders/models.py
@@ -28,8 +28,8 @@ class Order(models.Model):
     
 class OrderItem(models.Model):
     order = models.ForeignKey(Order, on_delete=models.CASCADE, related_name="items")
-    Products = models.ForeignKey(Products, on_delete=models.CASCADE)
-    quantity = models.ImageField()
+    Products= models.ForeignKey(Products, on_delete=models.CASCADE)
+    quantity = models.IntegerField()
     price = models.DecimalField(max_digits=12, decimal_places=2)
     created_at = models.DateField(auto_now_add=True)
 
