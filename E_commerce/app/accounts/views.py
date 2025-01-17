103501800716b3a7a429617d6cc23dd64027eb4b
Mahmoud <elmahallawy6@gmail.com>
2025-01-17 23:19:01 +0200

    change in users

--- E_commerce/app/accounts/views.py
+++ E_commerce/app/accounts/views.py
@@ -1,16 +1,15 @@
 from django.shortcuts import render
-#rest_framework related imports
-from rest_framework import viewsets, status, generics
+# Rest Framework related imports
+from rest_framework import viewsets, status
 from rest_framework.decorators import action
 from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated, AllowAny
-#JWT imports
+# JWT imports
 from rest_framework_simplejwt.views import TokenObtainPairView
 from django.contrib.auth import get_user_model
-from .serializer import UserCreateSerializer, UserSerializer, ChangePasswordSerializer,CustomTokenObtainPairSerializer
+from .serializer import UserCreateSerializer, UserSerializer, ChangePasswordSerializer, CustomTokenObtainPairSerializer
 
 User = get_user_model()
-
 
 class CustomTokenObtainPairView(TokenObtainPairView):
     serializer_class = CustomTokenObtainPairSerializer
@@ -21,7 +20,7 @@ class UserViewSet(viewsets.ModelViewSet):
     permission_classes = [IsAuthenticated]
 
     def get_permissions(self):
-        if self.action =="create":
+        if self.action == "create":
             return [AllowAny()]
         return super().get_permissions()
     
@@ -32,18 +31,19 @@ class UserViewSet(viewsets.ModelViewSet):
 
     @action(detail=False, methods=["get"])
     def me(self, request):
-        self.get_serializer(request.user)
+        # Create serializer instance with the current user
+        serializer = self.get_serializer(request.user)  # Fix: Store the serializer in a variable
         return Response(serializer.data)
 
     @action(detail=False, methods=["put"])
     def update_profile(self, request):
         user = request.user
-        serializer = UserSerializer(user, data=request.data, partial= True)
+        serializer = UserSerializer(user, data=request.data, partial=True)
         serializer.is_valid(raise_exception=True)
         serializer.save()
         return Response(serializer.data)
     
-    @action(detail=True, methods=["post"])
+    @action(detail=False, methods=["post"])
     def change_password(self, request):
         user = request.user
         serializer = ChangePasswordSerializer(data=request.data)
@@ -52,7 +52,7 @@ class UserViewSet(viewsets.ModelViewSet):
             return Response(
                 {"old_password": "wrong password"},
                 status=status.HTTP_400_BAD_REQUEST
-                    )
+            )
         user.set_password(serializer.validated_data['new_password'])
         user.save()
-        Response({"message": "password changed successfuly."})
+        return Response({"message": "password changed successfully."})  # Fix: Added return statement
